e67c718b8eb448d544bc03b68a0a1588e5ae750f
Maria Tran <maria.tran1996@outlook.com>
2024-03-24 14:30:35 -0400

    added question 5 part c

--- src/main/java/criteriaApi/JPACriteriaApi.java
+++ src/main/java/criteriaApi/JPACriteriaApi.java
@@ -10,6 +10,7 @@ import javax.persistence.PersistenceException;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
 import entity.AgeEntity;
@@ -22,21 +23,24 @@ public class JPACriteriaApi {
 		EntityTransaction entityTransaction = entityManager.getTransaction();
 
 		try {
-			//Begin Transaction
+			// Begin Transaction
 			entityTransaction.begin();
-            CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
-			
-			//QUESTION 5 - PART A
-            CriteriaQuery<GeographicAreaEntity> geoAreaCritQuery = criteriaBuilder.createQuery(GeographicAreaEntity.class);
-            Root<GeographicAreaEntity> geoArea = geoAreaCritQuery.from(GeographicAreaEntity.class);
-			MultiSelectTen(entityManager,geoAreaCritQuery,geoArea);
-			
-			//QUESTION 5 - PART B
+			CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+
+			// QUESTION 5 - PART A
+			CriteriaQuery<GeographicAreaEntity> geoAreaCritQuery = criteriaBuilder
+					.createQuery(GeographicAreaEntity.class);
+			Root<GeographicAreaEntity> geoArea = geoAreaCritQuery.from(GeographicAreaEntity.class);
+			MultiSelectTen(entityManager, geoAreaCritQuery, geoArea);
+
+			// QUESTION 5 - PART B
 			CriteriaQuery<AgeEntity> ageCritQuery = criteriaBuilder.createQuery(AgeEntity.class);
 			Root<AgeEntity> age = ageCritQuery.from(AgeEntity.class);
-			TopTwentyCombined(entityManager,criteriaBuilder,ageCritQuery,age);
-			
-			
+			TopTwentyCombined(entityManager, criteriaBuilder, ageCritQuery, age);
+
+			// QUESTION 5 - PART C
+			GetGeoAreaPeterborough(entityManager, criteriaBuilder, geoAreaCritQuery, geoArea);
+
 		} catch (PersistenceException pe) {
 			System.out.println("Error: " + pe.getMessage());
 		} catch (Exception ex) {
@@ -49,39 +53,54 @@ public class JPACriteriaApi {
 	}
 
 	// QUESTION 5 - PART A
-	public static void MultiSelectTen(EntityManager enMan, CriteriaQuery<GeographicAreaEntity> critQuery, Root<GeographicAreaEntity> geoArea) {
+	public static void MultiSelectTen(EntityManager enMan, CriteriaQuery<GeographicAreaEntity> critQuery,
+			Root<GeographicAreaEntity> geoArea) {
 
-		critQuery.multiselect(geoArea.get("code"),geoArea.get("level"),geoArea.get("name"));
-        CriteriaQuery<GeographicAreaEntity> multiSelect = critQuery.select(geoArea);
-        
-        //Display Output
-        TypedQuery<GeographicAreaEntity> query = enMan.createQuery(multiSelect).setMaxResults(10);
-        List<GeographicAreaEntity> geoAreaList = query.getResultList();
-        System.out.println("********** QUESTION 5 - PART A **********");
-        System.out.printf("%-20s %-20s %-20s %n", "Code", "Level", "Name");
-        geoAreaList.forEach(geo -> System.out.printf("%-20s %-20s %-20s %n",
-        		geo.getCode(), geo.getLevel(), geo.getName()));
+		critQuery.multiselect(geoArea.get("code"), geoArea.get("level"), geoArea.get("name"));
+		CriteriaQuery<GeographicAreaEntity> multiSelect = critQuery.select(geoArea);
+
+		// Display Output
+		TypedQuery<GeographicAreaEntity> query = enMan.createQuery(multiSelect).setMaxResults(10);
+		List<GeographicAreaEntity> geoAreaList = query.getResultList();
+		System.out.println("********** QUESTION 5 - PART A **********");
+		System.out.printf("%-20s %-20s %-20s %n", "Code", "Level", "Name");
+		geoAreaList.forEach(
+				geo -> System.out.printf("%-20s %-20s %-20s %n", geo.getCode(), geo.getLevel(), geo.getName()));
 
 	}
 
 	// QUESTION 5 - PART B
-	public static void TopTwentyCombined(EntityManager enMan, CriteriaBuilder critBuilder, CriteriaQuery<AgeEntity> critQuery, Root<AgeEntity> age) {
+	public static void TopTwentyCombined(EntityManager enMan, CriteriaBuilder critBuilder,
+			CriteriaQuery<AgeEntity> critQuery, Root<AgeEntity> age) {
 
 		critQuery.orderBy(critBuilder.desc(age.get("combined")));
-        CriteriaQuery<AgeEntity> orderBy = critQuery.select(age);
-        
-     // Display Output
-        TypedQuery<AgeEntity> query = enMan.createQuery(orderBy).setMaxResults(20);
-        List<AgeEntity> ageList = query.getResultList();
-        System.out.println("********** QUESTION 5 - PART B **********");
-        System.out.printf("%-20s %-20s %-20s %-20s %n", "AgeGroup", "CensusYear", "GeographicArea", "Combined");
-        ageList.forEach(ag -> System.out.printf("%-20s %-20s %-20s %-20s %n",
-        		ag.getAgeGroup(), ag.getCensusYear(), ag.getGeographicArea(), ag.getCombined()));
+		CriteriaQuery<AgeEntity> orderBy = critQuery.select(age);
+
+		// Display Output
+		TypedQuery<AgeEntity> query = enMan.createQuery(orderBy).setMaxResults(20);
+		List<AgeEntity> ageList = query.getResultList();
+		System.out.println("********** QUESTION 5 - PART B **********");
+		System.out.printf("%-20s %-20s %-20s %-20s %n", "AgeGroup", "CensusYear", "GeographicArea", "Combined");
+		ageList.forEach(ag -> System.out.printf("%-20s %-20s %-20s %-20s %n", ag.getAgeGroup(), ag.getCensusYear(),
+				ag.getGeographicArea(), ag.getCombined()));
 	}
 
 	// QUESTION 5 - PART C
-	public static void GetGeoAreaPeterborough() {
+	public static void GetGeoAreaPeterborough(EntityManager enMan, CriteriaBuilder critBuilder,
+			CriteriaQuery<GeographicAreaEntity> critQuery, Root<GeographicAreaEntity> geoArea) {
 
+		Predicate predicate = critBuilder.equal(geoArea.get("name"), "Peterborough");
+		critQuery.where(predicate);
+		CriteriaQuery<GeographicAreaEntity> whereClause = critQuery.select(geoArea);
+
+		// Display Output
+		TypedQuery<GeographicAreaEntity> query = enMan.createQuery(whereClause);
+		List<GeographicAreaEntity> geoList = query.getResultList();
+		System.out.println("********** QUESTION 5 - PART C **********");
+		System.out.printf("%-20s %-20s %-20s %-20s %-20s %n", "GeographicAreaID", "Code", "Level", "Name",
+				"AlternativeCode");
+		geoList.forEach(geo -> System.out.printf("%-20s %-20s %-20s %-20s %-20s %n", geo.getGeographicAreaID(),
+				geo.getCode(), geo.getLevel(), geo.getName(), geo.getAlternativeCode()));
 	}
 
 	// QUESTION 5 - PART D
